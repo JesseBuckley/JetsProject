a866e2761f888baa1d708370eea95d3d6f0487f7
JesseBuckley <jessebuckley2@gmail.com>
2023-12-01 18:49:35 -0500

    logic for Jet Abstract class done

--- src/com/skilldistillery/jets/Jet.java
+++ src/com/skilldistillery/jets/Jet.java
@@ -1,5 +1,18 @@
 package com.skilldistillery.jets;
 
 public abstract class Jet {
+	String model;
+	double speed;
+	int range;
+	long price;
+
+	public void fly() {
+		double flightTime = this.range / this.speed;
+		System.out.println("model " + this.model);
+		System.out.println("speed " + this.speed + "Mph");
+		System.out.println("range " + this.range);
+		System.out.println("price " + this.price);
+		System.out.println(flightTime);
+	}
 
 }
